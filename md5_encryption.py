489b4de48a55d9f86b83428f675b19b2e1f20a90
hemanth <hemanthwarrier@gmail.com>
2019-07-18 22:07:08 +0530

    updated

--- md5_encryption.py
+++ md5_encryption.py
@@ -1,33 +1,12 @@
 import hashlib 
   
 # initializing string 
-str = ["H HARIGOVIND",
-"Praful",
-"Sneha jayan",
-"Jibin George",
-"Prameela R",
-"REPHAH O",
-"Sara Reji",
-"Swathy Subash",
-"Ridhic Toms",
-"MOHAMMED AJMAL",
-"Anandu Ramachandran",
-"Abhishek Ajith",
-"Arya mohan",
-"shintu varghese",
-"Shimin C",
-"Ambadi s",
-"Adityan S",
-"V S ANANTHANARAYANAN",
-"SWATHY CHANDRA",
-"Adarsh v",
-"NA BALAMURALI",
-"ABHINDEV ES",
-"SREERAG VV",
-"VISHAK",
-"Yadhu Prasad",
-"Akshay VJ",
-"Devika Santhosh"]
+str = [
+'third Vishnu Prakash',
+'third Abraham Francis',
+'third Adwaith T',
+'third Srinivas R'
+]
   
 for name in str:
     result = hashlib.md5(name.encode()).hexdigest() 
